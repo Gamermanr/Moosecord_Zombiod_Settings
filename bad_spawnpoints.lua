b867a157015fc93bed1671042f91ff5f0da6d92c
Gamermanr <91105544+Gamermanr@users.noreply.github.com>
2026-08-13 18:46:30 -0400

    V1
    Signed-off-by: Gamermanr <91105544+Gamermanr@users.noreply.github.com>

---
+++ bad_spawnpoints.lua
@@ -0,0 +1,30 @@
+function SpawnPoints()
+	local sharedSpawns = {
+		{ worldX = 9, worldY = 36, posX = 62, posY = 153 }
+	}
+
+	return {
+		unemployed = sharedSpawns,
+		fireofficer = sharedSpawns,
+		policeofficer = sharedSpawns,
+		parkranger = sharedSpawns,
+		constructionworker = sharedSpawns,
+		securityguard = sharedSpawns,
+		carpenter = sharedSpawns,
+		burglar = sharedSpawns,
+		chef = sharedSpawns,
+		repairman = sharedSpawns,
+		farmer = sharedSpawns,
+		fisherman = sharedSpawns,
+		doctor = sharedSpawns,
+		veteran = sharedSpawns,
+		nurse = sharedSpawns,
+		lumberjack = sharedSpawns,
+		fitnessInstructor = sharedSpawns,
+		burgerflipper = sharedSpawns,
+		electrician = sharedSpawns,
+		engineer = sharedSpawns,
+		mechanic = sharedSpawns,
+		metalworker = sharedSpawns
+	}
+end
