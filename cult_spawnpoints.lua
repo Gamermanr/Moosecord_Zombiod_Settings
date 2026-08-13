b3917bb0d7a435dc9252df344d2e3a1e2385f7a6
Gamermanr <91105544+Gamermanr@users.noreply.github.com>
2026-08-13 18:36:24 -0400

    V1
    Signed-off-by: Gamermanr <91105544+Gamermanr@users.noreply.github.com>

---
+++ cult_spawnpoints.lua
@@ -0,0 +1,44 @@
+function SpawnPoints()
+	local sharedSpawns = {
+		{ worldX = 12, worldY = 40, posX = 190, posY = 277 },
+		{ worldX = 12, worldY = 40, posX = 190, posY = 269 },
+		{ worldX = 12, worldY = 40, posX = 190, posY = 256 },
+		{ worldX = 12, worldY = 40, posX = 190, posY = 248 },
+		{ worldX = 12, worldY = 40, posX = 203, posY = 236 },
+		{ worldX = 12, worldY = 40, posX = 210, posY = 237 },
+		{ worldX = 12, worldY = 40, posX = 224, posY = 237 },
+		{ worldX = 12, worldY = 40, posX = 231, posY = 237 },
+		{ worldX = 12, worldY = 40, posX = 246, posY = 237 },
+		{ worldX = 12, worldY = 40, posX = 252, posY = 237 },
+		{ worldX = 12, worldY = 41, posX = 211, posY = 14 },
+		{ worldX = 12, worldY = 41, posX = 213, posY = 11 },
+		{ worldX = 12, worldY = 41, posX = 218, posY = 13 },
+		{ worldX = 12, worldY = 41, posX = 218, posY = 4 },
+		{ worldX = 12, worldY = 41, posX = 212, posY = 6 }
+	}
+
+	return {
+		unemployed = sharedSpawns,
+		fireofficer = sharedSpawns,
+		policeofficer = sharedSpawns,
+		parkranger = sharedSpawns,
+		constructionworker = sharedSpawns,
+		securityguard = sharedSpawns,
+		carpenter = sharedSpawns,
+		burglar = sharedSpawns,
+		chef = sharedSpawns,
+		repairman = sharedSpawns,
+		farmer = sharedSpawns,
+		fisherman = sharedSpawns,
+		doctor = sharedSpawns,
+		veteran = sharedSpawns,
+		nurse = sharedSpawns,
+		lumberjack = sharedSpawns,
+		fitnessInstructor = sharedSpawns,
+		burgerflipper = sharedSpawns,
+		electrician = sharedSpawns,
+		engineer = sharedSpawns,
+		mechanic = sharedSpawns,
+		metalworker = sharedSpawns
+	}
+end
