8c6218c1298f642ecf78c216dd68a02b91153481
Gamermanr <91105544+Gamermanr@users.noreply.github.com>
2026-08-13 18:56:46 -0400

    V6
    Signed-off-by: Gamermanr <91105544+Gamermanr@users.noreply.github.com>

--- servertest_spawnregions.lua
+++ servertest_spawnregions.lua
@@ -4,7 +4,7 @@ function SpawnRegions()
 		{ name = "Main Town", serverfile = "echo_spawnpoints.lua" },
 		{ name = "Surrounding Forest", serverfile = "forest_spawnpoints.lua" },
 		{ name = "Farm Area", serverfile = "farmland__spawnpoints.lua" },
-		{ name = "Cults are not that bad", serverfile = "cult_spawnpoints.lua" },
+		{ name = "Cults aren't that bad", serverfile = "cult_spawnpoints.lua" },
 		{ name = "A bad idea", serverfile = "bad_spawnpoints.lua" },
 	}
 end
